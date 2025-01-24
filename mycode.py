ccbf2318340af715231825c4645d51d7f83e1edf
Yash <yashkhatri88540@gmail.com>
2025-01-24 13:21:34 +0530

    2nd version of data saved

--- mycode.py
+++ mycode.py
@@ -6,9 +6,12 @@ data= {
 }
 df =pd.DataFrame(data)
 
+new_row_loc= {'name': 'Gf1' , 'age':' 20'}
+df.loc[len(df.index)] = new_row_loc
 data_dir= 'data'
 os.makedirs(data_dir, exist_ok=True)
 file_path= os.path.join(data_dir,'sample.csv')
 
 df.to_csv(file_path, index=False)
 print(f"csv file saved to {file_path}")
+
